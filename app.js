39d0611cb1ebdf025ad60c75b49ffdbb9bac583a
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-15 17:03:31 +0800

    checkLoggedIn middleware

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ const app = express()
 
 app.use(helmet()) // add security middleware at the top of all mw and routes
 
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true // TODO
+    if (!isLoggedIn) {
+        return res.status(401).json({ error: 'log in required' })
+    }
+    next()
+
+}
+
+app.get('/secret', checkLoggedIn, (req, res) => {  // only check login for protected endpoint
+    return res.json({ msg: 'your personal secret value is 35' })
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 module.exports = app
+
