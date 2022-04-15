8488718ebc015e1abb7866291cab80261de12177
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-15 12:03:17 +0800

    helmet

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path = require('path')
+const helmet = require('helmet')
 
 const app = express()
+
+app.use(helmet()) // add security middleware at the top of all mw and routes
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
