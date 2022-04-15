e2ad47c2d3102e0110622ecc391ce44ea8dfd87b
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-15 17:37:42 +0800

    add google client id and secret in local config

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const https = require('https')
 const fs = require('fs')
+require('dotenv').config()
 
 const app = require('./app')
 const PORT = 3000
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET
+}
 
 https.createServer({
     key: fs.readFileSync('key.pem'),
