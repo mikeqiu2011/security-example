4c58dedde41ddc3ea605dc4546f1e796427a889b
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-16 09:17:44 +0800

    add req.isAuthenticated() to double confirm

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(passport.session())
 
 function checkLoggedIn(req, res, next) {
     console.log('current user is', req.user);
-    const isLoggedIn = req.user // TODO
+    const isLoggedIn = req.isAuthenticated() && req.user
     if (!isLoggedIn) {
         return res.status(401).json({ error: 'log in required' })
     }
